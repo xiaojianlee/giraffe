81baaef6700c98eaba82c63f7bc1575211707a8c
lixiaojian <lixiaojian-jk@360shuke.com>
2023-08-01 19:48:45 +0800

    提交表单-sql

---
+++ giraffe-infrastructure/src/main/resources/sql/init.sql
@@ -0,0 +1,1 @@
+INSERT INTO `sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `target`, `menu_type`, `visible`, `is_refresh`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (99, '小树木', 1, 10, '/tree/customView', '', 'C', '0', '1', 'system:user:view', 'fa fa-location-arrow', 'admin', '2023-08-01 15:09:22', '', NULL, '若依官网地址');
